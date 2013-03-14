d580614f8c8cced19abba998e881a97c8b530486
cgp <cgp@cgp-PC>
2013-03-14 12:54:22 +0800

    Change-Id: I8b0b76011866eb205952ef744a7e6a6e5b2b1bed

--- date.php
+++ date.php
@@ -1,7 +1,25 @@
 <?php
-$d1 = new DateTime('2013-03-14');
-$d2 = new DateTime('2012-12-12');
+$t1 = '2013-03-14';
+$t2 = '2012-12-12';
+$d1 = new DateTime($t1);
+$d2 = new DateTime($t2);
+$tz = $d1->getTimezone();
+echo '<h1>',$t1,',',$t2,'</h1>';
+echo 'time_zone='.$tz->getName(),'<br/>';
 echo 'date_diff='.$d1->diff($d2)->format('%R%a days'),'<br/>';
 echo $d1->sub(new DateInterval('P1Y2M1DT10H'))->format('Y-m-d H:i:s');
 echo '<br/>';
 echo $d1->add(new DateInterval('P1Y2M1DT1H'))->format('Y-m-d H:i:s');
+echo '<br/>';
+//getLastError
+try {
+	$d3 = new DateTime('abce');
+} catch (Exception $e) {
+	print_r(DateTime::getLastErrors());
+}
+//set Date
+$date = new DateTime();
+$date->setDate(1990, 1,2);
+$date->setTime(12,20);
+$date->modify('+1 years');
+echo '<h4>setDate:',$date->format('Y-m-d H:i:s'),'<h4>';
